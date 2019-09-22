5c988a53e4a6b93dfa5274a5e331710cb4545b36
leapingleo <jkl_4_sjk@hotmail.com>
2019-09-22 11:42:53 +1000

    Initial Commit

---
+++ opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl
+//
+//  Created by Leapingleo on 22/9/19.
+//  Copyright © 2019 Leapingleo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
